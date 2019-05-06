0e96bc1dd98b22c4f515d1be142fb6acfceda61d
Franklin Liou <fliou00@stuy.edu>
2019-05-06 13:18:36 -0400

    classwork

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -12,12 +12,50 @@ public class InsertionSorter extends Sorter {
     public InsertionSorter(  ArrayList< String> usersData) {
         // your code here, if necessary
     }
-    
+
 
     /**
       sort the user's data, implementing insertion sort
      */
     public void mySort() {
-        // your code here
+      int indexAlreadySorted = dweebIndex(elements, 0);
+      while (indexAlreadySorted <= elements.size()) {
+        insert1(elements, indexAlreadySorted + 1);
+        indexAlreadySorted++;
     }
+
+    public void insert1( ArrayList< String>, int alreadyInserted) {
+  		String valueToInsert = elements.get(alreadyInserted);
+  		if (valueToInsert < elements.get(0)) {
+  			elements.add(0, valueToInsert);
+  		}
+  		else {
+  			int indexToCompare = elements.size();
+  			while ( indexToCompare > 0) {
+  				if (valueToInsert.compareTo(elements[ indexToCompare]) < 0) {
+  					indexToCompare --;
+  				}
+  				else {
+  					elements.add(indexToCompare + 1, valueToInsert);
+  				}
+  			}
+  		}
+  	}
+
+  	// from hw62 solutions
+  	private int dweebIndex( ArrayList< String> list, int startAt) {
+          // use the starting element as a first guess
+          int dweebAt = startAt;
+          String dweeb = elements.get( dweebAt);
+
+          for( int testAt = startAt +1
+             ; testAt < elements.size()
+             ; testAt++)
+              if( elements.get( testAt).compareTo( dweeb) < 0) {
+                  // Found a smaller value. Remember it.
+                  dweebAt = testAt;
+                  dweeb = elements.get( dweebAt);
+              }
+          return dweebAt;
+  	}
 }
